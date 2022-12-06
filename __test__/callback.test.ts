c1faa5da5dddcb7cc0bd01591797f55e7a2a707a
Kisama <yelqgd@gmail.com>
2022-12-06 17:26:29 +0800

    refractor: add test case for cancel error

--- __test__/callback.test.ts
+++ __test__/callback.test.ts
@@ -3,7 +3,7 @@ import type { CallbackConfig } from '../src/types'
 import { describe, expect, it, vi } from 'vitest'
 import axios from 'axios'
 import axiosEnhancer from '../src/index'
-import { URL } from './utils'
+import { URL, sleep } from './utils'
 
 type D = {
   origin: string
@@ -194,6 +194,44 @@ describe('callback', () => {
     expect(callOnSuccess).toBeCalledTimes(0)
     expect(callOnFailed).toBeCalledTimes(1)
     expect(callOnError).toBeCalledTimes(0)
+    expect(callOnFinally).toBeCalledTimes(1)
+  })
+
+  it('error with cancel', async () => {
+    const enhancedAxiosIns = axiosEnhancer(axios.create())
+    const callOnBefore = vi.spyOn(cfg, 'onBefore')
+    const callOnSuccess = vi.spyOn(cfg, 'onSuccess')
+    const callOnFailed = vi.spyOn(cfg, 'onFailed')
+    const callOnError = vi.spyOn(cfg, 'onError')
+    const callOnFinally = vi.spyOn(cfg, 'onFinally')
+
+    const rPromise = enhancedAxiosIns.get(URL, {
+      adapter: async config => {
+        await sleep()
+
+        return Promise.resolve({
+          data: {
+            origin: 'hi'
+          },
+          status: 200,
+          statusText: 'ok',
+          headers: {},
+          config
+        })
+      },
+      $$userConfig: {
+        ...cfg,
+        needCancel: true
+      }
+    })
+
+    await sleep()
+    enhancedAxiosIns.$$ctlManager.abortAndRemove(URL)
+    await expect(rPromise).rejects.toThrow()
+    expect(callOnBefore).toBeCalledTimes(1)
+    expect(callOnSuccess).toBeCalledTimes(0)
+    expect(callOnFailed).toBeCalledTimes(0)
+    expect(callOnError).toBeCalledTimes(1)
     expect(callOnFinally).toBeCalledTimes(1)
   })
 })
